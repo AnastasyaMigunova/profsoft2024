5cd61baf98cd3582d86867c0a949693061624d63
AnastasyaMigunova <65sar.ru@gmail.com>
2024-07-15 00:41:00 +0300

    lesson3

---
+++ lesson1/app/src/main/java/com/example/profsoft2024/lesson3/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.profsoft2024.lesson3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.profsoft2024.R
+import com.example.profsoft2024.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding.twoTextsCity.twoTextsTitle.text = getString(R.string.city)
+        binding.twoTextsCity.twoTextsDescription.text = getString(R.string.city_value)
+
+        binding.twoTextsAboutMe.twoTextsTitle.text = getString(R.string.about_me)
+        binding.twoTextsAboutMe.twoTextsDescription.text = getString(R.string.about_me_value)
+    }
+}
